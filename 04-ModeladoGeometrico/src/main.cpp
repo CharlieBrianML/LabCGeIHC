51f178504b415ae0d376a8f35c35ce857b867aad
Charlie Brian <charls_brian@hotmail.es>
2019-09-05 17:15:09 -0500

    Practica 4

--- 04-ModeladoGeometrico/src/main.cpp
+++ 04-ModeladoGeometrico/src/main.cpp
@@ -15,11 +15,12 @@
 #include "Headers/Shader.h"
 
 // Model geometric includes
-/*
+
 #include "Headers/Sphere.h"
-#include "Headers/Cylinder.h"
+#include "Headers/Cylinder.h"///1
 #include "Headers/Box.h"
-*/
+#include "Headers/FirstPersonCamera.h"////2
+
 
 //GLM include
 #define GLM_FORCE_RADIANS
@@ -33,14 +34,16 @@ int screenHeight;
 GLFWwindow * window;
 
 Shader shader;
+std::shared_ptr<FirstPersonCamera> camera (new FirstPersonCamera());///2
 
-/*Sphere sphere1(20, 20);
-Cylinder cylinder1(20, 20, 0.5, 0.5);
-Box box1;*/
+Sphere sphere1(20, 20);
+Sphere sphere2(20, 20);
+Cylinder cylinder1(20, 20, 0.5, 0.5);//// 1
+Box box1;
 
 bool exitApp = false;
-int lastMousePosX;
-int lastMousePosY;
+int lastMousePosX, offsetX=0;
+int lastMousePosY, offsetY=0;
 
 double deltaTime;
 
@@ -107,18 +110,25 @@ void init(int width, int height, std::string strTitle, bool bFullScreen) {
 	glEnable(GL_CULL_FACE);
 
 	shader.initialize("../Shaders/colorShader.vs", "../Shaders/colorShader.fs");
-
-	/*sphere1.init();
+	//Inicializar los buffers VAO,VBO,EBO
+	sphere1.init();
+	//Metodo que setter que coloca el apuntador al shader
 	sphere1.setShader(&shader);
+	//Setter para poner el color de la geometria
 	sphere1.setColor(glm::vec4(0.3, 0.3, 1.0, 1.0));
-
+	//
 	cylinder1.init();
 	cylinder1.setShader(&shader);
 	cylinder1.setColor(glm::vec4(0.3, 0.3, 1.0, 1.0));
 
+	sphere2.init();
+	sphere2.setShader(&shader);
+	sphere2.setColor(glm::vec4(2.0, 0.5, 1.0, 1.0));////12
+
 	box1.init();
 	box1.setShader(&shader);
-	box1.setColor(glm::vec4(0.3, 0.3, 1.0, 1.0));*/
+	box1.setColor(glm::vec4(0.3, 0.3, 1.0, 1.0));////1
+	camera->setPosition(glm::vec3(2.0, 0.0, 4.0));
 }
 
 void destroy() {
@@ -126,10 +136,10 @@ void destroy() {
 	glfwTerminate();
 	// --------- IMPORTANTE ----------
 	// Eliminar los shader y buffers creados.
-
-	/*sphere1.destroy();
+	//Destruccion de los VAO,EBO,VBO
+	sphere1.destroy();
 	cylinder1.destroy();
-	box1.destroy();*/
+	box1.destroy();///1 
 
 	shader.destroy();
 }
@@ -151,8 +161,13 @@ void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mode
 }
 
 void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
-	lastMousePosX = xpos;
+	offsetX = xpos - lastMousePosX;///5
+	offsetY = ypos - lastMousePosY;
+	lastMousePosX = xpos;///5
 	lastMousePosY = ypos;
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {///5 
+		camera->mouseMoveCamera(offsetX, offsetY, deltaTime);
+	}
 }
 
 void mouseButtonCallback(GLFWwindow* window, int button, int state, int mod) {
@@ -179,7 +194,24 @@ bool processInput(bool continueApplication){
 
 	TimeManager::Instance().CalculateFrameRate(false);
 	deltaTime = TimeManager::Instance().DeltaTime;
+	if(glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS){///4  Nos podemos mover con la camara hacia enfrente
+		camera->moveFrontCamera(true, deltaTime);///4
+	}
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {///4  Nos podemos mover con la camara hacia atras
+		camera->moveFrontCamera(false, deltaTime);///4
+	}
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {///4  Nos podemos mover con la camara hacia la izquierda
+		camera->moveRightCamera(false, deltaTime);///4
+	}
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {///4  Nos podemos mover con la camara hacia la derecha
+		camera->moveRightCamera(true, deltaTime);///4
+	}
 
+	if (glfwGetMouseButton(window, GLFW_MOUSE_BUTTON_LEFT) == GLFW_PRESS) {/////5 Movimiento con el mouse
+		camera->mouseMoveCamera(offsetX, offsetY, 0.01);
+	}
+	offsetX = 0;///5
+	offsetY = 0;///5
 	glfwPollEvents();
 	return continueApplication;
 }
@@ -191,7 +223,8 @@ void applicationLoop() {
 		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 		glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float) screenWidth / (float) screenHeight, 0.01f, 100.0f);
-		glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+		//glm::mat4 view = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -3.0f));
+		glm::mat4 view = camera->getViewMatrix();///2
 
 		shader.turnOn();
 
@@ -200,16 +233,42 @@ void applicationLoop() {
 
 		glm::mat4 model = glm::mat4(1.0f);
 
-		/*sphere1.render(model);
-		sphere1.enableWireMode();*/
+		//Sirve para visualizar lineas
+		//sphere1.enableWireMode();////1
+		// Dibujado de la geometria y recibe la matriz de transicion
+		//sphere1.render(model);///1
 
 		/*cylinder1.render(model);
 		cylinder1.enableWireMode();*/
 
-		/*box1.render(model);
-		box1.enableWireMode();*/
-
+		box1.enableWireMode();///6
+		box1.render(glm::scale(model, glm::vec3(1.0, 1.0,0.1)));
+		//Articulacion1 
+		glm::mat4 j1 = glm::translate(model, glm::vec3(0.5f, 0.0f, 0.0f));////7
+		sphere1.enableWireMode();
+		sphere1.render(glm::scale(j1, glm::vec3(0.1, 0.1, 0.1)));////7
+		// Hueso 1
+		glm::mat4 l1 = glm::translate(j1, glm::vec3(0.25f, 0.0, 0.0));////8
+		l1 = glm::rotate(l1, glm::radians(90.0f),glm::vec3(0, 0, 1.0));
+		cylinder1.enableWireMode();
+		cylinder1.render(glm::scale(l1, glm::vec3(0.1, 0.5, 0.1)));////8
+		//Articulacion2 
+		glm::mat4 j2 = glm::translate(j1, glm::vec3(0.5, 0.0f, 0.0f));////9
+		sphere1.enableWireMode();
+		sphere1.render(glm::scale(j2, glm::vec3(0.1, 0.1, 0.1)));////9
+		// Hueso 2
+		glm::mat4 l2 = glm::translate(j2, glm::vec3(0.25, 0.0, 0.0));////10
+		l2 = glm::rotate(l2, glm::radians(90.0f), glm::vec3(0.0, 0.0, 1.0));
+		cylinder1.enableWireMode();
+		cylinder1.render(glm::scale(l2, glm::vec3(0.1, 0.5, 0.1)));////10
 		shader.turnOff();
+		//Ojos
+		glm::mat4 ojo = glm::translate(model, glm::vec3(0.25, 0.25, 0.05));//11
+		sphere1.enableWireMode();
+		sphere1.render(glm::scale(ojo, glm::vec3(0.2, 0.2, 0.1)));
+		glm::mat4 ojo2 = glm::translate(model, glm::vec3(-0.25, 0.25, 0.05));//11
+		sphere2.enableWireMode();
+		sphere2.render(glm::scale(ojo2, glm::vec3(0.2, 0.2, 0.1)));
 
 		glfwSwapBuffers(window);
 	}
